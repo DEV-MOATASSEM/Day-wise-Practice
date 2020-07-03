4fbd5c6954976d1d575ea03f72788fec936a428d
DEV-MOATASSEM <62389924+DEV-MOATASSEM@users.noreply.github.com>
2020-07-03 10:16:17 +0500

    Update Labled_Break.java

--- 28-04-2020/src/Labled_Break.java
+++ 28-04-2020/src/Labled_Break.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +10,17 @@
  * @author MOATASSEM SINDHU
  */
 public class Labled_Break {
+    public static void main(String[] args) {
+        Moatassem:
+        for(int i=0;i<=4;i++){
+            for(int j=0;j<=4;j++){
+                if (i==2)
+                    break Moatassem;
+               System.out.print(" *"); 
+            }
+            System.out.println();
+        }
+        
+    }
     
 }
